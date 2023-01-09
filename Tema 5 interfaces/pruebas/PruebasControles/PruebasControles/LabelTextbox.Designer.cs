ad8a3e20f79122e9991c7c00ab7b2242f3a3909c
MagicoDave <avcasalderrey@gmail.com>
2023-01-09 13:50:49 +0100

    Pruebas para el nuevo tema de interfaces

---
+++ Tema 5 interfaces/pruebas/PruebasControles/PruebasControles/LabelTextbox.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace PruebasControles
+{
+    partial class LabelTextbox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTxt = new System.Windows.Forms.Label();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTxt
+            // 
+            this.lblTxt.AutoSize = true;
+            this.lblTxt.Location = new System.Drawing.Point(47, 44);
+            this.lblTxt.Name = "lblTxt";
+            this.lblTxt.Size = new System.Drawing.Size(35, 13);
+            this.lblTxt.TabIndex = 0;
+            this.lblTxt.Text = "label1";
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(133, 41);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(100, 20);
+            this.txt.TabIndex = 1;
+            // 
+            // LabelTextbox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.lblTxt);
+            this.Name = "LabelTextbox";
+            this.Size = new System.Drawing.Size(299, 100);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTxt;
+        private System.Windows.Forms.TextBox txt;
+    }
+}
---
+++ Tema 5 interfaces/pruebas/PruebasControles/PruebasControles/LabelTextbox.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebasControles
+{
+
+    public enum ePosicion
+    {
+        IZQUIERDA, DERECHA
+    }
+
+    public partial class LabelTextbox: UserControl
+    {
+        public LabelTextbox()
+        {
+            InitializeComponent();
+        }
+
+        //Posición del label respecto al textbox
+        private ePosicion posicion = ePosicion.IZQUIERDA;
+        [Category("Appearance")]
+        [Description("Indica si la Label se situa a la IZQUIERDA o DERECHA del Textbox")]
+        public ePosicion Posicion
+        {
+            set
+            {
+                if (Enum.IsDefined(typeof(ePosicion), value))
+                {
+                    posicion = value;
+                    recolocar();
+                }
+                else
+                {
+                    throw new InvalidEnumArgumentException();
+                }
+            }
+            get
+            {
+                return posicion;
+            }
+        }
+
+        //Pixeles de separación entre label y textbox
+        private int separacion = 0;
+        [Category("Design")]
+        [Description("Píxels de separación entre Label y Textbox")]
+        public int Separacion
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    separacion = value;
+                    recolocar();
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            get
+            {
+                return separacion;
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("Texto asociado a la Label del control")]
+        public string TextLbl
+        {
+            set
+            {
+                lblTxt.Text = value;
+                recolocar();
+            }
+            get
+            {
+                return lblTxt.Text;
+            }
+        }
+        [Category("Appearance")]
+        [Description("Texto asociado al TextBox del control")]
+        public string TextTxt
+        {
+            set
+            {
+                txt.Text = value;
+            }
+            get
+            {
+                return txt.Text;
+            }
+        }
+
+        void recolocar() 
+        {
+            switch (posicion)
+            {
+                case ePosicion.IZQUIERDA:
+                    lblTxt.Location = new Point(0, 0); //Posicion lblTxT
+                    txt.Location = new Point(lblTxt.Width + Separacion, 0); //Posicion txt
+                    txt.Width = this.Width - lblTxt.Width - Separacion; //Ancho txt
+                    break;
+                case ePosicion.DERECHA:
+                    txt.Location = new Point(0, 0); //Posicion txt
+                    txt.Width = this.Width - lblTxt.Width - Separacion; //Ancho txt
+                    lblTxt.Location = new Point(txt.Width + Separacion, 0); //Posicion lblTxt
+                    break;
+            }
+
+            this.Height = Math.Max(txt.Height, lblTxt.Height); //Altura del componente
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            recolocar();
+        }
+    }
+}
